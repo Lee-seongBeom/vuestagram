129f133f6cb3b9fee96c3e14790db0db3fc0d016
SeongBeom <vnvnwn@openmate.co.kr>
2023-12-22 14:55:50 +0900

    탭 버튼으로 화면 제어 [2023-12-22]

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :게시물="게시물" />
-  <button @click="more" >더보기</button>
+  <Container :게시물="게시물" :tapCnt="tapCnt"/>
+  <button @click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
@@ -19,12 +19,18 @@
     </ul>
   </div>
 
+  <div v-if="tapCnt == 0">내용0</div>
+  <div v-if="tapCnt == 1">내용1</div>
+  <div v-if="tapCnt == 2">내용2</div>
+  <button @click="tapCnt = 0">버튼0</button>
+  <button @click="tapCnt = 1">버튼1</button>
+  <button @click="tapCnt = 2">버튼2</button>
 </template>
 
 <script>
 import Container from "./components/Container.vue";
 import data from "./data/data.js";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "App",
@@ -33,25 +39,27 @@ export default {
   },
   data() {
     return {
-      게시물 : data,
-      카운트 : 0,
+      게시물: data,
+      카운트: 0,
+      tapCnt : 0,
     };
   },
   methods: {
-    more(){
+    more() {
       // axios.post('url', {name: 'kim'}).then().catch((err)=>{
       //   console.log(err);
       // });
 
       // axios.get('https://codingapple1.github.io/vue/more'+this.카운트+'.json') // 같은 문법
-      axios.get(`https://codingapple1.github.io/vue/more${this.카운트}.json`) // es6 백틱기호 활용 (템플릿 리터럴)
-      .then((결과) => {
-        // 요청성공시 실행할 코드
-        console.log(결과.data);
-        this.게시물.push(결과.data);
-        this.카운트++;
-      })
-    }
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.카운트}.json`) // es6 백틱기호 활용 (템플릿 리터럴)
+        .then((결과) => {
+          // 요청성공시 실행할 코드
+          console.log(결과.data);
+          this.게시물.push(결과.data);
+          this.카운트++;
+        });
+    },
   },
 };
 </script>
